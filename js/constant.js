d9dcb1f2e92645b17f06af08fb373aacd09773e7
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-07-25 04:05:41 +0700

    portofolio not fix

---
+++ js/constant.js
@@ -0,0 +1,12 @@
+var LINK_HOME = "Beranda";
+var LINK_ABOUT = "Tentang";
+var LINK_RESUME = "Resume";
+var LINK_WORK = "Portofolio";
+var LINK_TESTIMONIALS = "Testimonials";
+var LINK_CONTACT = "Kontak";
+
+var HOME_NAME = "Aldi Wahyu Saragih";
+var HOME_CONTENT = "Programmer dibidang Mobile dan Website. Menggunakan bahasa pemrograman Java, JavaScript, Php5 (Native) dan memiliki paham tentang Database (MySql dan Firebase)";
+
+var ABOUT_TITLE = "Tentang Saya";
+var ABOUT_CONTENT = "";
